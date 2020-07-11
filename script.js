f0b60d9f7b437f565a9db72e29c64f989ed616a7
somaRe <shivatej82@gmail.com>
2020-07-11 12:00:08 +0530

    second

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-    var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/16PqM5t_b2ist3mp9l-OFAc5hNz9tAmOnB0KppxdHxwo/edit?usp=sharing';
 
-  function init() {
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/16PqM5t_b2ist3mp9l-OFAc5hNz9tAmOnB0KppxdHxwo/edit?usp=sharing';
+
+  $(document).ready(function(){
     Tabletop.init( {
       key: publicSpreadsheetUrl,
       callback: showInfo,
       simpleSheet: true 
     } )
-  }
+  
 
+  
   function showInfo(data, tabletop) {
-    var mi = document.getElementById('maininfo');
-    console.log(data);
     for(var i=0;i<data.length;i++){
-    mi.innerHTML += '<tr><td>'+data[i].Subject+'</td><td>'+data[i].Faculty+'</td><td>'+data[i].Slot+'</td><td><button type="button" class="btn btn-success"><a href="'+data[i].Whatsapp+'">Link</a></button></td><tr>'
-    }
+    $( "tbody" ).append('<tr><td>'+data[i].Subject+'</td><td>'+data[i].Faculty+'</td><td>'+data[i].Slot+'</td><td><button type="button" class="btn btn-success"><a href="'+data[i].Whatsapp+'">Link</a></button></td><tr>');
   }
-
-  window.addEventListener('DOMContentLoaded', init)
-
-  $(document).ready(function(){
+  }
+  
     $("#search").on("keyup", function() {
       var value = $(this).val().toLowerCase();
       $("#maininfo tr").filter(function() {
